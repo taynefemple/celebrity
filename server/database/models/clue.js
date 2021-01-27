c8686c8343c306805a0fc787fd48468ff12acf56
Wayne Temple <waynetemple@gmail.com>
2021-01-26 21:56:20 -0600

    removed explicit id attribute from clue

--- server/database/models/clue.js
+++ server/database/models/clue.js
@@ -2,12 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('../database');
 
 const Clue = db.define('clue', {
-    // do I need this id?
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
     name: {
         type: Sequelize.STRING,
         allowNull: false,
