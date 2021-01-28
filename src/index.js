96ed682d61eb0786bbd0852cb63a2033c3ea1450
Wayne Temple <waynetemple@gmail.com>
2021-01-27 21:28:08 -0600

    adding event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-const celebSubmit = document.querySelector('.add-celeb');
-celebSubmit.addEventListener('click', () => console.log('Celebrity Added!'));
+// index.html
+// submit button
+// const celebSubmit = document.querySelector('.add-celeb');
+// celebSubmit.addEventListener('click', function (evt) {
+//     console.log('Celebrity Added!')
+// });
 
+// // Input field
+// const celebInput = document.querySelector('#celeb');
+// celebInput.addEventListener('keyup', function (evt) {
+//     console.log(`Celebrity Being entered!!!!  ${evt.key}`)
+// });
+
+// // Start Game Button
+// const startGame = document.querySelector('#start-game');
+// startGame.addEventListener('click', function (evt) {
+//     console.log('Let\'s PLAY!!!!')
+// });
+
+// rounds1and3.html
+// Round Start Button
+const celeb = document.querySelector('.celeb');
+
+const startRound = document.querySelector('#round-start');
+startRound.addEventListener('click', function (evt) {
+    console.log('Let\'s PLAY!!!!');
+    celeb.innerHTML = 'John Jacaob Jingelheimer schmidt';
+});
