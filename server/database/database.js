44b64c0dc6cbbc013712de1d23c0151c60cbbe2e
Wayne Temple <waynetemple@gmail.com>
2021-02-14 14:35:55 -0600

    setting up heroku db config for ssl

--- server/database/database.js
+++ server/database/database.js
@@ -4,19 +4,16 @@ const pkg = require('../../package.json');
 const databaseName =
     pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
+function getSslConfig (){
+    return process.env.NODE_ENV === 'production'
+}
+
 const db = new Sequelize(
     process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
     // for heroku app db is 'postgresql-curly-60237'
     {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        ssl: process.env.DB_ENABLE_SSL,
-        dialectOptions: {
-            ssl: process.env.DB_ENABLE_SSL && {
-                require: true
-            }
-        },
         logging: false,
+        ssl: getSslConfig()
     }
 );
 module.exports = db;
