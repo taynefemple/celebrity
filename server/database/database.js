39dc80a4c85b17faedbe49927702e37b977ff31f
Wayne Temple <waynetemple@gmail.com>
2021-02-14 12:20:04 -0600

    tweaking db setup for heroku

--- server/database/database.js
+++ server/database/database.js
@@ -6,7 +6,16 @@ const databaseName =
 
 const db = new Sequelize(
     process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+    // for heroku app db is 'postgresql-curly-60237'
     {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        ssl: process.env.DB_ENABLE_SSL,
+        dialectOptions: {
+            ssl: process.env.DB_ENABLE_SSL && {
+                require: true
+            }
+        },
         logging: false,
     }
 );
